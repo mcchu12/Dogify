4f2c09c8daca5a992bf6ad802aa036360a3dc620
Michael Chu <mcchu95@gmail.com>
2018-10-27 21:05:15 -0400

    Add scroll effect to navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+
 import { faBars, faEllipsisV, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -16,12 +18,20 @@ export class HeaderComponent implements OnInit {
   icMenu: IconDefinition;
   icMore: IconDefinition;
 
-  constructor() {
+  heroHeight: number;
+  windowWidth: number;
+
+  navDark: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.heroHeight = window.innerHeight / 2 - 80;
+    this.windowWidth = window.innerWidth;
   }
 
   ngOnInit() {
     this.icMenu = faBars;
     this.icMore = faEllipsisV;
+    console.log('Hero height: ', this.heroHeight);
   }
 
   // Check if nav has overlay class before adding expanding class
@@ -29,6 +39,15 @@ export class HeaderComponent implements OnInit {
     if (this.nav.nativeElement.classList.contains('nav')) {
       this.menuToggle = !this.menuToggle;
     }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const pos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    // Check scroll position and window width
+    if (pos > this.heroHeight && this.windowWidth < 960) {
+      this.navDark = true;
+    } else { this. navDark = false; }
   }
 
 }
