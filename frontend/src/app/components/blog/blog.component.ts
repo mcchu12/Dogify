5583ae8584efedc7258bf4c26f130603eb76c214
Michael Chu <mcchu95@gmail.com>
2018-11-11 00:43:23 -0500

    Add blog grid

--- frontend/src/app/components/blog/blog.component.ts
+++ frontend/src/app/components/blog/blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,12 +6,13 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent implements OnInit, AfterViewInit {
 
   posts: any;
   first: any;
+  postItem: Element;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private el: ElementRef) { }
 
   ngOnInit() {
     this.http.get('http://localhost:5000/api/blog').subscribe(
@@ -23,4 +24,28 @@ export class BlogComponent implements OnInit {
     );
   }
 
+  ngAfterViewInit(): void {
+    this.resizeAllGridItems();
+  }
+
+  private resizeGridItem(item: HTMLElement) {
+    const grid = this.el.nativeElement.querySelector('.post-container');
+    const gridAttr = window.getComputedStyle(grid);
+    const rowHeight = parseInt(gridAttr.getPropertyValue('grid-auto-rows'), 10);
+    console.log('rowHeight', rowHeight);
+    const rowGap = parseInt(gridAttr.getPropertyValue('grid-row-gap'), 10);
+    console.log('rowGap', rowGap);
+    const contentHeight = item.querySelector('.post-content').getBoundingClientRect().height;
+    const rowSpan = Math.ceil((contentHeight + rowGap) / (rowHeight + rowGap));
+    console.log(contentHeight);
+    console.log('rowSpan', rowSpan);
+    item.style.gridRowEnd = 'span ' + rowSpan;
+  }
+
+  private resizeAllGridItems() {
+   const items = this.el.nativeElement.querySelectorAll('.post');
+  items.forEach(element => {
+    this.resizeGridItem(element);
+  });
+  }
 }
